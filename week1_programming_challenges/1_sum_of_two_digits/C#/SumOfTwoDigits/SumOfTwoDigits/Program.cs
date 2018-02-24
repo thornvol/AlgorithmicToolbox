1a92a1caa21047749b216968f94e16c568d422e0
Thornton <tthornto@stjude.org>
2018-02-24 15:06:29 -0600

    Added all files

---
+++ week1_programming_challenges/1_sum_of_two_digits/C#/SumOfTwoDigits/SumOfTwoDigits/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfTwoDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            if (input != null)
+            {
+                var values = input.Split(' ');
+                var a = Convert.ToInt64(values[0]);
+                var b = Convert.ToInt64(values[1]);
+                var product = a + b;
+                Console.Write(product);
+            }
+            
+            //Console.ReadLine();
+        }
+    }
+}
